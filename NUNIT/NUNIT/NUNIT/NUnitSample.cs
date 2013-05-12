4e2641fb4446cba8ba4791ce421592f8e3bacc90
lincolnge <326684793@qq.com>
2013-05-12 23:09:38 +0800

    NUnit sample update

---
+++ NUNIT/NUNIT/NUNIT/NUnitSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NUnitSample
+{
+    public class MathUtil
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
